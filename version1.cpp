6bd547e43f5fb3a717f1d64f8ddab589ac98bd8a
snakedata <62944294+snakedata@users.noreply.github.com>
2023-10-03 16:12:59 +0200

    Update version1.cpp

--- version1.cpp
+++ version1.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <cmath>
 #include <vector>
+
+
+//1-11 17- end
 using namespace std;
 //External libraries
 //GSL
@@ -10,24 +13,35 @@ using namespace std;
 
 //Twenty lines per function maximum
 //Pointer arithmetic forbidden
-
+//output.write(reninterpret_cast<char*>(&x),sizeof(x)); //NOLINT(..)
 //Vectores iniciales - fuerzas resultantes -
+//Unique pointer or shared point
 
+//Vector of blocks where each block is a list or vector of particles
 
-struct particle{
-    float px;
-    float py;
-    float pz;
-    float hvx;
-    float hvy;
-    float hvz;
-    float vx;
-    float vy;
-    float vz;
-    float p = 0;
-    float a = 9.81;
+struct Particle{
+    double px;
+    double py;
+    double pz;
+    double hvx;
+    double hvy;
+    double hvz;
+    double vx;
+    double vy;
+    double vz;
+    double a = 9.81;
+    double p = 0;
 };
-double distance_squared(particle p1, particle p2){
+struct Block{
+    int x;
+    int y;
+    int z;
+    vector<Particle> particles;
+};
+
+
+
+double distance_squared(Particle p1, Particle p2){
     double dx = p1.px - p2.px;
     double dy = p1.py - p2.py;
     double dz = p1.pz - p2.pz;
@@ -36,6 +50,46 @@ double distance_squared(particle p1, particle p2){
 };
 
 int main(int argc, char** argv) {
+
+    float ppm_double;
+    int np;
+
+    //Parte de lectura de Juan crear todas las particulas
+    ifstream file("/Users/raulpineda/iCloud Drive (Archive)/Document/Arquitectura/Lab1/small.fld", ios::binary);
+
+    if (!file.is_open()) {
+        cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+
+    // Read ppm and np
+
+    //cap 10-11
+    file.read(reinterpret_cast<char*>(&ppm_double), sizeof(float));
+    file.read(reinterpret_cast<char*>(&np), sizeof(int));
+
+    std::cout << "ppm: " << ppm_double << ", np: " << np << std::endl;
+
+    // Create a vector to store particles
+    std::vector<Particle> particles;
+
+    for (int i = 0; i < np; ++i) {
+        Particle particle;
+        file.read(reinterpret_cast<char*>(&particle), sizeof(Particle));
+        particles.push_back(particle);
+    }
+    double ppm = static_cast<float>(ppm_double);
+    file.close();
+    //int counter;
+    // Access particles
+    //for (const Particle& particle : particles) {
+    //    std::cout << "Particle Data:" << counter  << std::endl;
+    //    std::cout << "px: " << particle.px << ", py: " << particle.py << ", pz: " << particle.pz << std::endl;
+    //    std::cout << "hvx: " << particle.hvx << ", hvy: " << particle.hvy << ", hvz: " << particle.hvz << std::endl;
+    //    std::cout << "vx: " << particle.vx << ", vy: " << particle.vy << ", vz: " << particle.vz << std::endl;
+    //    counter +=1;
+    //}
+
     //Contsants intialization
     double r = 1.695;
     double p = 1000;
@@ -46,69 +100,124 @@ int main(int argc, char** argv) {
     double dp = 0.0002;
     double time_step = 0.001;
     int particle_num = 2;
-    double ppm = 2;
-    double mass = p*pow(ppm,3);
+
+    double m = p*pow(ppm,3);
     double h = r/ppm;
 
 
-
-
-    vector<particle> vector_particles;
-
-    particle particle1{};
-    particle1.px = 2;
-    particle1.py = 2;
-    particle1.pz = 2;
-    particle1.hvx = 2;
-    particle1.hvy = 2;
-    particle1.hvz = 2;
-    particle1.vx = 2;
-    particle1.vy = 2;
-    particle1.vz = 2;
-
-    particle particle2{};
-    particle2.px = 2;
-    particle2.py = 1.99;
-    particle2.pz = 2;
-    particle2.hvx = 2;
-    particle2.hvy = 2;
-    particle2.hvz = 2;
-    particle2.vx = 2;
-    particle2.vy = 2;
-    particle2.vz = 2;
-
-
-    //Parte de lectura de Juan crear todas las particulas
-    vector_particles.push_back(particle1);
-    vector_particles.push_back(particle2);
-
-
     //Parte del grid creation
-    int b_min[3] = {1,2,3};
-    int b_max[3] = {4,5,6};
-    int boxx = b_max[0] - b_min[0];
-    int boxy = b_max[1] - b_min[1];
-    int boxz = b_max[2] - b_min[2];
 
-    //Density computation
-    double d;
-    for (auto i = vector_particles.begin();i !=vector_particles.end(); i++){
-        for (auto j = i++;j != vector_particles.end(); j++){
-            d = distance_squared(*i,*j);
-            if (d < h){
-                auto density_inc =pow((h*h-d),3);
-                auto increment = static_cast<float>(density_inc);
-                cout << increment;
-                i->p = i->p + increment;
-                j->p += i->p +increment;
+    double b_min[3] = {-0.065,-0.08,-0.065};
+    double b_max[3] = {0.065,0.1,0.065};
+    double boxx = b_max[0] - b_min[0];
+    double boxy = b_max[1] - b_min[1];
+    double boxz = b_max[2] - b_min[2];
+
+    m = p/pow(ppm,3);
+    h = r/ppm;
+    cout << boxx;
+
+
+    int nx = floor(boxx/h);
+    int ny = floor(boxy/h);
+    int nz = floor(boxz/h);
+    cout << "\n r: " << r << " ppm: " << ppm;
+    double sx = boxx/h;
+    double sy = boxy/h;
+    double sz = boxz/h;
+    int NumberofBlocks = (nx-1)*(ny-1)*(nz-1);
+
+
+
+
+    //Placing particles in blocks
+    //Create a grid which is made of blocks
+    std::vector<Block> grid;
+    grid.reserve(NumberofBlocks);
+    int counterBlock= 0;
+
+    cout << "\nnx " << nx << " ny " << ny << " nz " << nz << " Number of blocks " << NumberofBlocks;
+
+    for (int x = 0; x < nx-1; x++){
+        for (int y = 0; y < ny-1; y++){
+            for (int z = 0; z < nz-1; z++){
+                counterBlock+=1;
+                cout << "\nThis is block:" <<  counterBlock << " x value: " << x << " y value: " << y << " z value: " << z;
+                Block block;
+                block.x = x;
+                block.y = y;
+                block.z = z;
+                cout << 1;
+                grid.push_back(block);
+                cout << 0;
             }
         }
     }
-    cout << particle1.p;
-    cout << particle2.p;
+    //Grid organization
+    cout << "\n Grid organization";
+    int blockPositionx;
+    int blockPositiony;
+    int blockPositionz;
+    int blockNumber;
+    int counter = 0;
+    for (auto particle = particles.begin();particle!=particles.end();particle++){
+        blockPositionx = floor(particle->px/sx);
+        blockPositiony = floor(particle->py/sy);
+        blockPositionz = floor(particle->pz/sz);
+        blockNumber = blockPositionx*blockPositiony + blockPositiony*blockPositionz + blockPositionz;
+        cout << "This is particle number: " << counter << " px " << blockPositionx << " py " << blockPositiony << " pz " << blockPositionz << " Block number" << blockNumber;
+        counter +=1;
+        //For every y there are nz number of z blocks and for every x there are ny * nz number of blocks
 
-    //Reposicionamiento de las acceleraciones
-    //Verification of distance
+        grid[blockNumber].particles.push_back(*particle);
+    }
+
+
+
+
+
+    //Density computation
+    double d;
+    double i_density;
+    double j_density;
+    double density_inc;
+    double acceleration_inc;
+    double density_constant_transformation_part = 64*M_PI*pow(h,9);
+    double densityConstantTransformation =  315 * m / density_constant_transformation_part;
+
+    for (auto i = particles.begin();i !=particles.end(); i++){
+        for (auto j = i +1;j != particles.end(); j++){
+            d = distance_squared(*i,*j);
+
+            if (d < h){
+                //Density
+                density_inc =pow((h*h-d),3);
+                i_density = i->p + density_inc;
+                j_density = i->p +density_inc;
+                //Linear transformation
+                i->p =(i_density + pow(h,6))*densityConstantTransformation;
+                //Check in optimization
+                j->p =(j_density + pow(h,6))*densityConstantTransformation;
+                //Acceleration
+                //Local to each iteration
+                //Check formula later
+                i->hvx = i->hvx + ((i->px-j->px)*15*m*(h-d)*(h-d)*(i->p+j->p -p)+45*(i->vx-j->vx)*nu*m)/(M_PI*pow(h,6)*i->p*j->p);
+                i->hvy = i->hvy + ((i->py-j->py)*15*m*(h-d)*(h-d)*(i->p+j->p -p)+45*(i->vy-j->vy)*nu*m)/(M_PI*pow(h,6)*i->p*j->p);
+                i->hvz = i->hvz + ((i->pz-j->pz)*15*m*(h-d)*(h-d)*(i->p+j->p -p)+45*(i->vz-j->vz)*nu*m)/(M_PI*pow(h,6)*i->p*j->p);
+            }
+
+        }
+    }
+
+//Collisions
+
+
+
+//Write floats use standard conversion static cast or narrow cast
+
+
+
+
 
 
 }
